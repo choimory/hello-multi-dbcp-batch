75eaad59da20e6cccae9b780653615c07bcb9ef7
choimory <choimory@naver.com>
2021-06-21 19:41:41 +0900

    Initial commit

---
+++ src/main/java/com/choimory/hello/hellomultidbcpbatch/HelloMultiDbcpBatchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.hello.hellomultidbcpbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMultiDbcpBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMultiDbcpBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/hello/hellomultidbcpbatch/HelloMultiDbcpBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.hello.hellomultidbcpbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMultiDbcpBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
